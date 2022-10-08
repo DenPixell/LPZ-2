2839916578292d189275cbfac37e25f880204985
Den Pixel <werty1045@outlook.com>
2022-10-08 18:03:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,8 @@
+﻿using ConsoleApp3;
+
+Person per1 = new Person("per1", "num1", new DateTime(2000, 01, 35));
+Person per2 = new Person("per2", "num2", new DateTime(2000, 1102, 002));
+Console.WriteLine(per1.GetHashCode());
+Console.WriteLine(per2.GetHashCode());
+
+
